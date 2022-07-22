4a051d6c2e713cdbe4e44b49317e3bc7947d1571
Omar Loyo <omarloyo121@gmail.com>
2022-07-21 20:03:30 -0600

    added firebase to client and created config file

---
+++ client/src/firebase/config_sample.js
@@ -0,0 +1,21 @@
+// THIS IS JUST A SAMPLE
+// Create a copy named config.js and put proper credentials
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
