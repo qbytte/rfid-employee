070e2ee2b02a146c125afd5ec814d0dc2700e0a2
Omar Loyo <omarloyo121@gmail.com>
2022-07-21 20:11:18 -0600

    added moment js to firebase dependencies and created cloud function to calculate how long an employee has worked based on log start and finish time

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,9 +1,23 @@
 const functions = require("firebase-functions");
+const moment = require("moment");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.onLogCreate = functions.database
+  .ref("/logs/{logId}")
+  .onCreate((snapshot, context) => {
+    const logId = context.params.logId;
+    console.log(logId);
+
+    const logData = snapshot.val();
+    const start = moment(logData.start, "HH:mm:ss");
+    const finish = moment(logData.finish, "HH:mm:ss");
+
+    const duration = moment.duration(finish.diff(start));
+
+    const hrs = parseInt(duration.asHours());
+    const min = parseInt(duration.asMinutes()) % 60;
+    const sec = parseInt(duration.asSeconds()) % 60;
+
+    return snapshot.ref.update({
+      hrsWorked: moment(`${hrs}:${min}:${sec}`, "HH:mm:ss").format("HH:mm:ss"),
+    });
+  });
