e2caae1c8a80eda02038921e0ad9084f7b56fb26
Omar Loyo <omarloyo121@gmail.com>
2022-07-23 22:54:27 -0600

    finished footer and header, added react icons for icons

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,9 +1,14 @@
+import { IoIosArrowBack } from "react-icons/io";
 import "./styles.css";
 
 const Header = ({ title }) => {
   return (
     <div className="Header">
-      {!title && <button className="Header-back">Placeholder</button>}
+      {!title && (
+        <button className="Header-back">
+          <IoIosArrowBack size={40} color={"#FFFDFD"} />
+        </button>
+      )}
       <h1 className="Header-title">{title}</h1>
     </div>
   );
