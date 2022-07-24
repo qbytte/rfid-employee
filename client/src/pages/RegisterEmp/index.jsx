73c87c307dddb30c8d0de935023c12f60eea277c
Omar Loyo <omarloyo121@gmail.com>
2022-07-24 17:27:19 -0600

    moved pages into their own folder and created style files for each one

---
+++ client/src/pages/RegisterEmp/index.jsx
@@ -0,0 +1,13 @@
+import { Header, Form } from "../../components";
+import "./styles.css"
+
+const RegisterEmp = () => {
+  return (
+    <div>
+      <Header title={"Register new employee"} />
+      <Form />
+    </div>
+  );
+};
+
+export default RegisterEmp;
