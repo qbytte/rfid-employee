21848813a8528641a5ed86a466cd6ec9e2552450
Omar Loyo <omarloyo121@gmail.com>
2022-07-26 20:18:43 -0600

    updated firebase config_sample with needed sdks and exports

--- client/src/firebase/config_sample.js
+++ client/src/firebase/config_sample.js
@@ -3,6 +3,8 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +21,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
+
+// Initialize Cloud Storage and get a reference to the service
+const storage = getStorage(app);
+
+export { database, storage }
