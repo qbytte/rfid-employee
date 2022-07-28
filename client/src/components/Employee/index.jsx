5bc901e4a1d7ab511932cbce9ad08ca70e5af417
Omar Loyo <omarloyo121@gmail.com>
2022-07-28 01:17:56 -0600

    finished component styles

--- client/src/components/Employee/index.jsx
+++ client/src/components/Employee/index.jsx
@@ -1,26 +1,22 @@
+import "./style.css";
+
 const Employee = ({ imgUrl, atWork, firstName, lastName, id }) => {
-    return (
-        <div className='Employee'>
-            <div>
-                <div className="Employee-photo">
-                    <img src={imgUrl} />
-                </div>
-                <p className="Employee-status">Status: {atWork}</p>
-            </div>
-            <div>
-                <p className='Employee-name'>{`${firstName} ${lastName}`}</p>
-                <p className='Employee-id'>{id}</p>
-            </div>
+  return (
+    <div className={atWork ? "Employee Employee-online" : "Employee"}>
+      <div className="Employee-first">
+        <div className="Employee-photo">
+          <img src={imgUrl} />
         </div>
-    )
-}
+        <p className="Employee-status">
+          Status: {atWork ? "Online" : "Offline"}
+        </p>
+      </div>
+      <div className="Employee-second">
+        <p className="Employee-name">{`${firstName} ${lastName}`}</p>
+        <p className="Employee-id">{id}</p>
+      </div>
+    </div>
+  );
+};
 
-export default Employee
-
-// dibs
-// mamadas y chingadas
-// contenedores y chingaderas
-// dos contenedores padre
-// primer container imagen p, para Offline
-// para seg names de p etselente
-// 
+export default Employee;
