406b6ac3c3445295959e8921f6cc5e4a961a6f33
Richizero <richizero@hotmail.com>
2022-07-27 20:10:56 -0600

    finished component structure

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,3 +2,4 @@ export { default as Header } from "./Header";
 export { default as Layout } from "./Layout";
 export { default as Form } from "./Form";
 export { default as EditPhoto } from "./EditPhoto";
+export { default as LogComponent } from "./LogComponent"
