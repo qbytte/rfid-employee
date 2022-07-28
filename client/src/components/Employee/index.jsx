6ee5c9675efa4b03ad76a0ecfbfaa36701ee75c2
satanxxzero <slavadorgamboa09@gmail.com>
2022-07-27 20:52:03 -0600

    Fineshed component structure

---
+++ client/src/components/Employee/index.jsx
@@ -0,0 +1,27 @@
+const Employee = () => {
+    return (
+        <div className='Employee'>
+            <div>
+                <div className="Employee-photo">
+                    <img/>
+                </div>
+                <p className="Employee-status">Status: online</p>
+            </div>
+            <div>
+                <p className='Employee-name'>nombre</p>
+                <p className='Employee-id'>id</p>
+            </div>
+        </div>
+    )
+}
+
+
+export default Employee
+
+// dibs
+// mamadas y chingadas
+// contenedores y chingaderas
+// dos contenedores padre
+// primer container imagen p, para Offline
+// para seg names de p etselente
+// 
