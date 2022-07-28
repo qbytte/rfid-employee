59a2ac4ce25d8853f11b2a1ef76b43f393ae138f
satanxxzero <slavadorgamboa09@gmail.com>
2022-07-27 20:17:05 -0600

    erased layout and moved footer to components folder

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,4 @@
 export { default as Header } from "./Header";
-export { default as Layout } from "./Layout";
 export { default as Form } from "./Form";
 export { default as EditPhoto } from "./EditPhoto";
+export { default as Footer} from "./Footer";
