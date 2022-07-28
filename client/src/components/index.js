916b6f0e0e869a0366a948c61488b3066beb7665
Omar Loyo <omarloyo121@gmail.com>
2022-07-28 01:17:56 -0600

    finished component styles

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Header } from "./Header";
 export { default as Form } from "./Form";
+export { default as Employee } from "./Employee"
 export { default as EditPhoto } from "./EditPhoto";
 export { default as Footer} from "./Footer";
