f8f3eb6e0961b5a55d7289ddc8e817fc7e22d13c
Omar Loyo <omarloyo121@gmail.com>
2022-07-29 22:16:37 -0600

    list of employees working, sort pending

--- client/src/pages/ActiveEmps/index.jsx
+++ client/src/pages/ActiveEmps/index.jsx
@@ -1,33 +1,51 @@
 import { useState, useEffect } from "react";
-import { Footer } from "../../components";
+import { Employee, Footer } from "../../components";
 import { database } from "../../firebase/config";
-import { ref, onValue } from "firebase/database";
+import { ref, onValue, set } from "firebase/database";
 
 const ActiveEmployees = () => {
   const [emps, setEmps] = useState([]);
+
   const fetchData = () => {
     const employeesRef = ref(database, "employees/");
     onValue(employeesRef, (snapshot) => {
       const data = snapshot.val();
-      console.log(data);
-      const array = Object.entries(data).map(entry => {
-        return { [entry[0]]: entry[1] };
-      });
+      const array = [];
+
+      for (const [key, value] of Object.entries(data)) {
+        array.push({
+          id: key,
+          firstName: value.firstName,
+          lastName: value.lastName,
+          atWork: value.atWork,
+          imgUrl: value.imgUrl,
+        });
+      }
       setEmps(array);
-    })
-  }
+    });
+  };
 
   useEffect(() => {
     fetchData();
-  }, [])
+  }, []);
+
   return (
     <div>
-      { emps.map((emp) => { 
-        console.log(emp)
+      {emps.map((emp) => {
+        return (
+          <Employee
+            id={emp.id}
+            firstName={emp.firstName}
+            lastName={emp.lastName}
+            atWork={emp.atWork}
+            imgUrl={emp.imgUrl}
+            key={emp.id}
+          />
+        );
       })}
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default ActiveEmployees
+export default ActiveEmployees;
