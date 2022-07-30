a3a4fd43b342d2c4e045914f8f415d321844d86b
Arturo Muela <eltorturo93@gmail.com>
2022-07-29 23:57:08 -0600

    updated strcuture of LogComponent

--- client/src/components/LogComponent/Index.jsx
+++ client/src/components/LogComponent/Index.jsx
@@ -3,13 +3,13 @@ import "./styles.css";
 const LogComponent = ({ id, shiftstart, shiftend, date }) => {
   return (
     <div className="LogComponent">
-      <p>{id}</p>
-      <span className="LogComponent-span">•</span>
-      <p>{shiftstart} </p>
-      <span className="LogComponent-span">•</span>
-      <p>{shiftend}</p>
-      <span className="LogComponent-span">•</span>
-      <p>{date} </p>
+      <div>{id}</div>
+      <div className="LogComponent-span">•</div>
+      <div>{shiftstart} </div>
+      <div className="LogComponent-span">•</div>
+      <div>{shiftend}</div>
+      <div className="LogComponent-span">•</div>
+      <div>{date} </div>
     </div>
   );
 };
