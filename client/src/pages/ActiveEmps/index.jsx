9dbe51a25a1344a37ba6ccf7b759c7a4a99d06a6
satanxxzero <slavadorgamboa09@gmail.com>
2022-07-29 20:34:01 -0600

    started active emps page, db connection stablished

--- client/src/pages/ActiveEmps/index.jsx
+++ client/src/pages/ActiveEmps/index.jsx
@@ -1,8 +1,30 @@
-import { Footer } from "../../components"
+import { useState, useEffect } from "react";
+import { Footer } from "../../components";
+import { database } from "../../firebase/config";
+import { ref, onValue } from "firebase/database";
+
 const ActiveEmployees = () => {
+  const [emps, setEmps] = useState([]);
+  const fetchData = () => {
+    const employeesRef = ref(database, "employees/");
+    onValue(employeesRef, (snapshot) => {
+      const data = snapshot.val();
+      console.log(data);
+      const array = Object.entries(data).map(entry => {
+        return { [entry[0]]: entry[1] };
+      });
+      setEmps(array);
+    })
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
   return (
     <div>
-      <p>ActiveEmployees</p>
+      { emps.map((emp) => { 
+        console.log(emp)
+      })}
       <Footer />
     </div>
   )
