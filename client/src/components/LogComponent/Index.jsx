ade3c3063a870c779eac0dde150ba5e44a10e3e2
Omar Loyo <omarloyo121@gmail.com>
2022-08-02 20:21:06 -0600

    fixed conflict

---
+++ client/src/components/LogComponent/Index.jsx
@@ -0,0 +1,23 @@
+
+const LogComponent = ({ id, shiftstart, shiftend, date }) => {
+  return (
+    <div>
+
+      <p>
+        123456
+      </p>
+      <p>
+        00:00
+      </p>
+      <p>
+        08:00
+      </p>
+      <p>
+        27/07/2022
+      </p>
+
+    </div>
+  )
+}
+
+export default LogComponent
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,3 +3,4 @@ export { default as Form } from "./Form";
 export { default as Employee } from "./Employee"
 export { default as EditPhoto } from "./EditPhoto";
 export { default as Footer} from "./Footer";
+export { default as LogComponent } from "./LogComponent"
