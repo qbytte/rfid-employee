9bc1ef3ed7fdfa9ea31523cd5913833ba4dc4a04
Omar Loyo <omarloyo121@gmail.com>
2022-08-02 20:20:09 -0600

    added navigation to log component, when clicked it takes to emp details related to log

--- client/src/components/LogComponent/index.jsx
+++ client/src/components/LogComponent/index.jsx
@@ -1,15 +1,18 @@
+import { useLocation } from "wouter";
 import "./styles.css";
 
 const LogComponent = ({ id, shiftstart, shiftend, date }) => {
+  const [_location, setLocation] = useLocation();
+
   return (
-    <div className="LogComponent">
+    <div onClick={() => setLocation(`/details/${id}`)} className="LogComponent">
       <div>{id}</div>
       <div className="LogComponent-span">•</div>
-      <div>{shiftstart} </div>
+      <div>{shiftstart}</div>
       <div className="LogComponent-span">•</div>
       <div>{shiftend}</div>
       <div className="LogComponent-span">•</div>
-      <div>{date} </div>
+      <div>{date}</div>
     </div>
   );
 };
