f61722963ec8a3aff9bdc509d97827fbd209fd04
Omar Loyo <omarloyo121@gmail.com>
2022-08-02 18:17:43 -0600

    created container for logs, styled screen in general

--- client/src/pages/EmpLogs/index.jsx
+++ client/src/pages/EmpLogs/index.jsx
@@ -1,12 +1,22 @@
-import { Footer } from "../../components"
+import { Header, LogComponent, Footer } from "../../components";
+import "./styles.css";
 
 const Logs = () => {
   return (
     <div>
-      <p>Logs</p>
+      <Header title="Employee logs" />
+      <div className="Emp-Logs-header">
+        <div>ID</div>
+        <div>Shift start</div>
+        <div>Shift end</div>
+        <div>Date</div>
+      </div>
+      <div className="EmpLogs-container">
+        
+      </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Logs
+export default Logs;
