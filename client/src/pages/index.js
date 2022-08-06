dadd8267d9e8a02a4b1031fcf571351d8c192a81
Omar Loyo <omarloyo121@gmail.com>
2022-08-05 21:04:43 -0600

    implemented hours worked in emp detail screen, removed dedicated screen to this feature since it wasn't needed

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,4 +3,3 @@ export { default as EditEmp } from "./EditEmp";
 export { default as EmpDetails } from "./EmpDetails";
 export { default as EmpLogs } from "./EmpLogs";
 export { default as RegisterEmp } from "./RegisterEmp";
-export { default as WorkedHours } from "./WorkedHours";
