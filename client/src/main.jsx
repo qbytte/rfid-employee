7318df6a663c38e6ee32285a39548f89c6c029af
Omar Loyo <omarloyo121@gmail.com>
2022-08-09 15:06:27 -0600

    service worker using vite

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { registerSW } from "virtual:pwa-register";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      manifest: {
+        name: "RFID-Employee",
+        short_name: "R-EMP",
+        description: "Management app for your employees",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+        ],
+      },
+      strategies: "generateSW"
+    }),
+  ],
+});
