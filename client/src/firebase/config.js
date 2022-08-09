e840d2ca5ea2b69ec8a224d34825a5c47fc638c7
Omar Loyo <omarloyo121@gmail.com>
2022-08-09 15:21:26 -0600

    added firebase config credential to env

--- client/src/firebase/config.js
+++ client/src/firebase/config.js
@@ -1,22 +1,16 @@
-// THIS IS JUST A SAMPLE
-// Create a copy named config.js and put proper credentials
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
@@ -28,4 +22,4 @@ const database = getDatabase(app);
 // Initialize Cloud Storage and get a reference to the service
 const storage = getStorage(app);
 
-export { database, storage }
+export { database, storage };
